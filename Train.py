63d1aeeeea88c58f2b182cf6e1e0cb397d6ebbc2
MohFarahani <80159489+MohFarahani@users.noreply.github.com>
2021-09-13 23:08:28 +0200

    black format

--- Train.py
+++ Train.py
@@ -104,20 +104,30 @@ class Train:
             )
         elif self.config.MODEL == "autoencoder":
             encoder_inputs = tf.keras.layers.Input(shape=(self.input_size))
-            encoder_l1 = tf.keras.layers.LSTM(100,return_sequences = True, return_state=True)
+            encoder_l1 = tf.keras.layers.LSTM(
+                100, return_sequences=True, return_state=True
+            )
             encoder_outputs1 = encoder_l1(encoder_inputs)
             encoder_states1 = encoder_outputs1[1:]
             encoder_l2 = tf.keras.layers.LSTM(100, return_state=True)
             encoder_outputs2 = encoder_l2(encoder_outputs1[0])
             encoder_states2 = encoder_outputs2[1:]
             #
-            decoder_inputs = tf.keras.layers.RepeatVector(self.config.HORIZON)(encoder_outputs2[0])
+            decoder_inputs = tf.keras.layers.RepeatVector(self.config.HORIZON)(
+                encoder_outputs2[0]
+            )
             #
-            decoder_l1 = tf.keras.layers.LSTM(100, return_sequences=True)(decoder_inputs,initial_state = encoder_states1)
-            decoder_l2 = tf.keras.layers.LSTM(100, return_sequences=True)(decoder_l1,initial_state = encoder_states2)
-            decoder_outputs2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(self.input_size[1]))(decoder_l2)
+            decoder_l1 = tf.keras.layers.LSTM(100, return_sequences=True)(
+                decoder_inputs, initial_state=encoder_states1
+            )
+            decoder_l2 = tf.keras.layers.LSTM(100, return_sequences=True)(
+                decoder_l1, initial_state=encoder_states2
+            )
+            decoder_outputs2 = tf.keras.layers.TimeDistributed(
+                tf.keras.layers.Dense(self.input_size[1])
+            )(decoder_l2)
             #
-            model = tf.keras.models.Model(encoder_inputs,decoder_outputs2)
+            model = tf.keras.models.Model(encoder_inputs, decoder_outputs2)
 
         model.compile(optimizer="adam", loss="mse", metrics=["mae"])
         model.summary()
